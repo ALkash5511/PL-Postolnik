91767d8d1a59fc7a0ff3d1aedde33f8c06925782
ALkash5511 <postolnik07@bk.ru>
2024-11-28 19:38:34 +0300

    Add files via upload

---
+++ Практика/Практическая 10/Z10.py
@@ -0,0 +1,3 @@
+with open('C:\\Users\\ZOV_SVO\\Desktop\\vvod.txt', 'r') as file_1:
+    with open('C:\\Users\\ZOV_SVO\\Desktop\\vivod.txt', 'w') as file_2:
+        file_2 = file_2.write(file_1.read())
